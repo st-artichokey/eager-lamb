4a63c4fa7c9297b26ac6b067e3e2cee8f0ebc372
Rosanne Ussery <russery@russery-ltmpy49.internal.salesforce.com>
2025-09-03 10:55:27 -0500

    got bolt up yay

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 import bolt from "@slack/bolt"
-import axios from "axios";
+import axios from "axios"
+import { config } from "dotenv";
+// import { registerListeners } from "./listeners/index.js";
 
-const { App, LogLevel } = require('@slack/bolt');
-const { config } = require('dotenv');
-const { registerListeners } = require('./listeners');
 const { App } = bolt
 
 config();
@@ -11,14 +10,13 @@ config();
 /** Initialization */
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
   socketMode: true,
   appToken: process.env.SLACK_APP_TOKEN,
-  logLevel: LogLevel.DEBUG,
-  signingSecret: process.env.SLACK_SIGNING_SECRET
 });
 
-/** Register Listeners */
-registerListeners(app);
+// /** Register Listeners */
+// registerListeners(app);
 
 /** Start the Bolt App */
 (async () => {
@@ -34,5 +32,33 @@ registerListeners(app);
 async function getUselessFact() {
   const res = await axios.get("https://uselessfacts.jsph.pl/api/v2/facts/random");
   return (res.status === 200)? res.data : null;
-  
 }
+
+// Define the function for Workflow Builder
+
+app.function("useless_fact_step", async ( { complete, fail, logger } ) => {
+
+  try {
+    app.logger.info( "Running useless fact step…" );
+
+    // Get the fact using the API
+    const fact = await getUselessFact();
+    if ( fact && fact.text ) {
+      // Use complete() to send results to Slack
+      await complete( {
+        outputs: {
+          message: fact.text
+        }
+      } );
+
+    } else {
+      // Use fail() to send an error to Slack
+      await fail( { error: "There was an error retrieving a random useless fact :cry:" } );
+    }
+
+  } catch ( error ) {
+    // Log the error and send an error message back to Slack
+    logger.error( error );
+    await fail( { error: `There was an error 😥 : ${ error }` } );
+  }
+} );
